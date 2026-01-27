e3bb59ab97687c73497262d0c2cad9bc1aeb7402
Yudai Ito <yudai.ito.9124@gmail.com>
2026-01-27 17:31:56 +0900

    fix file path

--- frontend/lib/config.ts
+++ frontend/lib/config.ts
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
     APP_URL: process.env.NEXT_PUBLIC_APP_URL || 'https://master.d19yct597o7t9y.amplifyapp.com',
     API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://cs8f9x08p5.execute-api.ap-northeast-1.amazonaws.com/prod',
-    QR_LOGO_PATH: '/gakuchousenbei169.jpeg',
+    QR_LOGO_PATH: '/presenticon.png',
 };
