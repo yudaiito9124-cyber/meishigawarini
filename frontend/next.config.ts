1578aec9ec3003f178883abc2391512ced188c00
yudai ito <yudai.ito.9124@gmail.com>
2026-02-10 12:38:22 +0900

    fix env. valiables

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  },
   images: {
     remotePatterns: [
       {
